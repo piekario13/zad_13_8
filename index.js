8d1f958aca9030182e23bbc288010a9d333dbcb9
piekario13 <piekario13@gmail.com>
2017-12-02 23:45:00 +0100

    serwer HTTP

---
+++ index.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('./index.html', 'utf-8', function(err, html) {
+            if (err) throw err;
+            response.write(html);
+            response.end();
+        });
+    } else {
+            response.statusCode = 404;
+            response.setHeader('Content-Type', 'image/jpg');
+            fs.readFile('./404.jpg', function(err, img) {
+            if (err) throw err;
+            response.end(img);
+        });
+    
+    }
+});
+
+server.listen(8080);
